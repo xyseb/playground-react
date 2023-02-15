8ca135d7555d537edad6ff396cb15d5c31680cf3
Sebastien Deschamps <sdeschamps@alpes-deis.fr>
2023-02-15 08:48:17 +0100

    Add dotnet api.

---
+++ src/api/Models/Centre.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Centre
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
