4d45da2f7e81938669bb45aa96b49fe200affc99
Sebastien Deschamps <sdeschamps@alpes-deis.fr>
2023-02-16 08:53:30 +0100

    Add dotnet API + json-server data api + start script.

---
+++ src/api/Models/CentreContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Models
+{
+    public class CentreContext : DbContext
+    {
+        public CentreContext(DbContextOptions<CentreContext> options) : base(options) {
+
+        }
+
+        public DbSet<Centre> Centre { get; set; }        
+        public DbSet<CentreParams> Params { get; set; }        
+    }
+}
