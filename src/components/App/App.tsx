b1262a492f77b1d991b276af0410d54dd9ff829b
Sebastien Deschamps <sdeschamps@alpes-deis.fr>
2023-02-23 12:01:48 +0100

    Backup during redux like stores implementation (big cleanning)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
 
 import './App.scss';
 import { Routes, Route, NavLink } from 'react-router-dom';
 
 import HomePage from '../Pages/HomePage/HomePage';
-import CentreContextPage from '../Pages/CentreContextPage/CentreContextPage';
 import NotFoundPage from '../Pages/NotFoundPage/NotFoundPage';
 import ReduxStorePage from '../Pages/ReduxStorePage/ReduxStorePage';
 import RtkStorePage from '../Pages/RtkStorePage/RtkStorePage';
-import CentreContextProvider from '../../contexts/CentreContextProvider';
 
 import { reduxStore } from '../../stores/redux/ReduxStore'
-//import { rtkStore } from '../../stores/rtk/RtkStore'
 import { Provider } from 'react-redux'
 
-import ReactQueryPage from '../Pages/ReactQueryPage/ReactQueryPage';
-import { SWRConfig } from 'swr';
-import SwrPage from '../Pages/SwrPage/SwrPage';
-
-const isDev = (process.env.NODE_ENV === 'development') ? true : false;
-const queryClient = new QueryClient({ 
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: !isDev
-    }
-  }
-})
 
 function App() {
   
@@ -36,38 +19,19 @@ function App() {
       <header>
         <nav>
           <NavLink className={({ isActive }) => isActive ? 'nav is-active' : 'nav' } to="/home"><span>HOME</span></NavLink>
-          <NavLink className={({ isActive }) => isActive ? 'nav is-active' : 'nav' } to="/ctx"><span>CTX</span></NavLink>
-          <NavLink className={({ isActive }) => isActive ? 'nav is-active' : 'nav' } to="/rq"><span>RQ</span></NavLink>
-          <NavLink className={({ isActive }) => isActive ? 'nav is-active' : 'nav' } to="/swr"><span>SWR</span></NavLink>
           <NavLink className={({ isActive }) => isActive ? 'nav is-active' : 'nav' } to="/redux"><span>REDUX</span></NavLink>
-          <NavLink className={({ isActive }) => isActive ? 'nav is-active' : 'nav' } to="/rtk"><span>RTK</span></NavLink>
-          <NavLink className={({ isActive }) => isActive ? 'nav is-active' : 'nav' } to="/kui"><span>KUI</span></NavLink>
-          {/* <NavLink className={({ isActive }) => isActive ? 'nav is-active' : 'nav' } to="/404"><span>404</span></NavLink> */}
         </nav>
       </header>
-      <SWRConfig value={{ provider: () => new Map(), fetcher: (url:string) => fetch(url).then(res => res.json()) }}>
-      <QueryClientProvider client={queryClient}>
       <Provider store={reduxStore}>
-        <CentreContextProvider>
         <section>
           <Routes>
             <Route path="/" element={<HomePage/>}>
               <Route path="/home" element={<HomePage/>}/>
             </Route>
-            <Route path="/ctx" element={<CentreContextPage/>}/>
             <Route path="/redux" element={<ReduxStorePage/>}/>
-            <Route path="/rtk" element={<RtkStorePage/>}/>
-            <Route path="/rq" element={<ReactQueryPage/>}/>
-            <Route path="/swr" element={<SwrPage/>}/>
-            <Route path="/kui" element={<NotFoundPage/>}/>
-            {/* <Route path="/404" element={<NotFoundPage/>}/> */}
           </Routes>
         </section>    
-        </CentreContextProvider>
       </Provider>
-      <ReactQueryDevtools initialIsOpen={isDev} />
-      </QueryClientProvider>
-      </SWRConfig>
     </main>
   );
 }
--- src/rq-api/rqApi.ts.md
+++
@@ -1,3 +0,0 @@
-export function getNomCentre() {
-    () => fetch('http://localhost:8080/centre').then(res => res.json())
-};
